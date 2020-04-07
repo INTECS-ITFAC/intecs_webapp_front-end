0bba57a575f041f0bb9046b4872c04165f434071
ChanakaUOMIT <chanakauomfit@gmail.com>
2020-04-08 01:03:50 +0530

    event, project, about basic structure implemented

--- src/modules/about/About.tsx
+++ src/modules/about/About.tsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 // import PropTypes from "prop-types";
-
+import "./About.scss";
 export class About extends Component {
   static propTypes = {};
 
   render() {
     return (
-      <div>
-        <div>About us</div>
-        <div>Allocation space</div>
+      <div className="about-container">
+        <div>About us Allocation space</div>
       </div>
     );
   }
--- src/modules/project/Project.tsx
+++ src/modules/project/Project.tsx
@@ -1,26 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+// import PropTypes from 'prop-types'
+import "./Project.scss";
 
-const About = () => {
-  return (
-    <div>
-      <div className="App">
-        <header className="App-header">
-          <h1>About</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+export class Project extends Component {
+  static propTypes = {};
+
+  render() {
+    return (
+      <div className="project-container">
+        <div>Project Allocated Space</div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default About;
+export default Project;
