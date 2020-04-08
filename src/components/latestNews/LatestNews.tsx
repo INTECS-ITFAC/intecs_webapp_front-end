2957b04fc1c382817c95ed31e865970681dd421e
Kelum Sampath Edirisinghe <edirisingheemks@gmail.com>
2020-04-08 09:03:14 +0530

    prettier test commit

--- src/components/latestNews/LatestNews.tsx
+++ src/components/latestNews/LatestNews.tsx
@@ -6,6 +6,9 @@ function LatestNews() {
   return (
     <div className="latest-news-container">
       <div>Latest News Allocated Space</div>
+      <button className="prettier-class" id="prettier-id" onClick={() => {}}>
+        Click Here
+      </button>
     </div>
   );
 }
