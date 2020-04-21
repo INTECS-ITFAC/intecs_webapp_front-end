866d038943d40d07e51588aa7bf776d4222cce52
Sahan Amarsha <sahanamarsha@gmail.com>
2020-04-21 20:23:37 +0530

    Here I am seperating components 'AboutUs' and 'ContactUs'

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 //import PropTypes from "prop-types";
+import { Row, Col, Card } from "react-bootstrap";
 import "./Footer.scss";
 
 function Footer() {
   return (
-    <div className="footer-container">
-      <div>Footer allocated space</div>
+    <div className="footer-container d-inline">
+      <Row className="text-center" style={{ background: "#005476" }}>
+        <Col style={{ color: "#D3D3D3" }}>
+          Developed by : OpenSource Community
+        </Col>
+      </Row>
     </div>
+    // <div className="footer-container">
+    //     <p className='align-content-center'>
+    //         Developed By : OpenSource Community
+    //     </p>
+    //
+    // </div>
   );
 }
 
