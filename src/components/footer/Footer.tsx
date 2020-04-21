3d05b56c50e16d2e167b3f9ff3598aee2acd208a
Sahan Amarsha <sahanamarsha@gmail.com>
2020-04-22 02:01:44 +0530

    Editing the social media bar

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,13 +2,16 @@ import React from "react";
 //Component imports
 import Contactus from "../contactus/Contactus";
 import Aboutus from "../aboutus/Aboutus";
-
 //Style imports
-import { Row, Col, Badge } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import Switch from "./Switch";
 import "./Footer.scss";
 
+//Social Icons
+
 class Footer extends React.Component<{}, { mode: string }> {
+  // const facebookUrl = "www.facebook.com";
+
   constructor(props: any) {
     super(props);
     //I have used a state to switch between two modes 'ContactUs' and 'AboutUs'
@@ -44,7 +47,57 @@ class Footer extends React.Component<{}, { mode: string }> {
         </Row>
         {this.state.mode === "AboutUs" ? <Contactus /> : <Aboutus />}
         {/*Part 2 - this will include only text content*/}
-        <div className="footer-part2"></div>
+
+        <div className="footer-part2">
+          <ul className="list-unstyled list-inline text-center">
+            <li className="list-inline-item">
+              <a className="btn-floating btn-tw mx-1">
+                <img
+                  src={require("../../assets/images/social_icons/facebook.png")}
+                  className="socialIcon"
+                  alt="Facebook"
+                />
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a className="btn-floating btn-tw mx-1">
+                <img
+                  src={require("../../assets/images/social_icons/dribble.png")}
+                  className="socialIcon"
+                  alt="Dribble"
+                />
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a className="btn-floating btn-gplus mx-1">
+                <img
+                  src={require("../../assets/images/social_icons/linkedin.png")}
+                  className="socialIcon"
+                  alt="Linkedin"
+                />
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a className="btn-floating btn-li mx-1">
+                <img
+                  src={require("../../assets/images/social_icons/twitter.png")}
+                  className="socialIcon"
+                  alt="Twitter"
+                />
+              </a>
+            </li>
+            <li className="list-inline-item">
+              <a className="btn-floating btn-dribbble mx-1">
+                <img
+                  src={require("../../assets/images/social_icons/youtube.png")}
+                  className="socialIcon"
+                  alt="Youtube"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+
         <div className="bottom-footer-container d-inline">
           <Row className="text-center" style={{ background: "#005476" }}>
             <Col style={{ color: "#D3D3D3" }}>
