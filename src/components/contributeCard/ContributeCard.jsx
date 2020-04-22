b88681443185d1e86885796e8484cf66694453ec
JayaShakthi97 <jayashakthiperera@gmail.com>
2020-04-22 16:12:22 +0530

    Contributor's profile card #41

--- src/components/contributeCard/ContributeCard.jsx
+++ src/components/contributeCard/ContributeCard.jsx
@@ -1,24 +1,28 @@
 import React from "react";
+import "./ContributeCard.scss";
 
 export default function ContributeCard({ data }) {
   console.log("ContributeCard -> data", data);
   return (
-    <div className="col-sm-6 col-md-4 col-lg-3 mt-4">
-      <div className="card">
-        <img alt="user" className="card-img-top" src={data.avatar_url} />
-        <div className="card-block">
-          <h4 className="card-title">{data.login}</h4>
-          <div class="btn btn-success">
-            <a href={data.html_url} target="_blank" rel="noopener noreferrer">
+    <div className="contribute-card-div col-xl-2 col-lg-3 col-md-4 col-sm-5">
+      <div className="card contribute-card">
+        <div className="card-body">
+          <div className="profile">
+            <img src={data.avatar_url} className="thumbnail" alt={data.login} />
+            <h3 className="name">{data.login}</h3>
+            <div>
+              <h4>{data.contributions}</h4>
+              <p>Contributions</p>
+            </div>
+            <a
+              href={data.html_url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="profile-button"
+            >
               Profile
             </a>
           </div>
-        </div>
-        <div className="card-footer">
-          <span>
-            <i className />
-            {data.contributions} contributions
-          </span>
         </div>
       </div>
     </div>
