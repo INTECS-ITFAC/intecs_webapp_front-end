3b869b860a4297f7e6ad488e766ce74b86df9243
SadheeraSMahanama <sadheeramahanama94@gmail.com>
2020-04-23 16:43:56 +0530

    Dynamic carousel was implemented.

--- src/components/homeCarousel/HomeCarousel.tsx
+++ src/components/homeCarousel/HomeCarousel.tsx
@@ -1,14 +1,35 @@
-import React, {useState} from "react";
-// import PropTypes from 'prop-types'
+import React, {useState} from "react"; 
 import "./HomeCarousel.scss";
 import Carousel from 'react-bootstrap/Carousel';
-// import Image1 from "../../assets/images/carousel/image1.jpg";
-const Image1 = require('../../assets/images/carousel/image1.jpg');
-const Image2 = require('../../assets/images/carousel/image2.jpg');
-const Image3 = require('../../assets/images/carousel/image3.jpg');
-const Image4 = require('../../assets/images/carousel/image4.jpg');
-const Image5 = require('../../assets/images/carousel/image5.jpg');
+ 
+const data = [
+  {
+   image: require('../../assets/images/carousel/image1.jpg'), 
+   caption:"Caption",
+   description:"Description Here"
+  },
+  {
+    image:require('../../assets/images/carousel/image2.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   },
+   {
+    image:require('../../assets/images/carousel/image3.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   },
+   {
+    image:require('../../assets/images/carousel/image4.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   },
+   {
+    image:require('../../assets/images/carousel/image5.jpg'), 
+    caption:"Caption",
+    description:"Description Here"
+   },
 
+]
 
 function HomeCarousel() {
   const [index, setIndex] = useState(0);
@@ -16,79 +37,28 @@ function HomeCarousel() {
     setIndex(selectedIndex);
   };
   return (
-    <div className="home-carousel-container">      
-      {/* <div>Home Carousel Allocated space</div> */}
+    <div className="home-carousel-container">        
       <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>        
+      {data.map((slide, i) => {
+        return (
+          <Carousel.Item>        
         <img
           className="d-block w-100"
-          src={String(Image1)}
+          src={String(slide.image)}
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>{slide.caption}</h3>
+          <p>{slide.description}</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={String(Image2)}
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={String(Image3)}
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        
-        <img
-          className="d-block w-100"
-          src={String(Image4)}
-          alt="Forth slide"
-        />
-        <Carousel.Caption>
-          <h3>Forth slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item>        
-        <img
-          className="d-block w-100"
-          src={String(Image5)}
-          alt="Fifth slide"
-        />
-        <Carousel.Caption>
-          <h3>Fifth slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+        )
+      })}
+      
     </Carousel>
     </div>
   );
  
 }
-
-// HomeCarousel.propTypes = {
-
-// }
-
+ 
 export default HomeCarousel;
