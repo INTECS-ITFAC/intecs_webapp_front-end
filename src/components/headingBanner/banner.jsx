3d9a85009f86a62b4a9adac77a71793581c06c4a
Pulsara Sandeepa <pulsarasandeepa@gmail.com>
2020-04-23 14:35:24 +0530

    headingBanner for Eventpage

---
+++ src/components/headingBanner/banner.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Banner from "react-js-banner";
+import "./banner.css";
+import logo from "./logo/frog.png";
+
+class Heading extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      name: "Blog",
+      bannerCss: { color: "white", fontSize: "50px", marginTop: "150px" },
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Banner
+          image={logo}
+          imageClass="App-logo"
+          title={[
+            <div>
+              <h1 className="jumbotron-fluid" style={this.state.bannerCss}>
+                {this.state.name}
+              </h1>
+              <div>
+                <p className="lead" style={{ fontFamily: "Segoe UI" }}>
+                  A blog project by Ben
+                </p>
+              </div>
+            </div>,
+          ]}
+        />
+      </div>
+    );
+  }
+}
+
+export default Heading;
--- src/modules/event/Event.tsx
+++ src/modules/event/Event.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import PropTypes from 'prop-types'
 import "./Event.scss";
+import Heading from "../../components/headingBanner/banner";
 
 export class Event extends Component {
   static propTypes = {};
@@ -8,6 +9,9 @@ export class Event extends Component {
   render() {
     return (
       <div className="event-container">
+        <div>
+          <Heading />
+        </div>
         <div>Allocated Space for Event </div>
       </div>
     );
