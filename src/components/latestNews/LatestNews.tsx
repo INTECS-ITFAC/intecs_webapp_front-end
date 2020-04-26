14f5bdfb67c786ac6a2c96e2969f710194d5e610
Pulsara Sandeepa <pulsarasandeepa@gmail.com>
2020-04-26 14:01:09 +0530

    create the News Feed component in Homepage

--- src/components/latestNews/LatestNews.tsx
+++ src/components/latestNews/LatestNews.tsx
@@ -1,17 +1,34 @@
 import React from "react";
+// @ts-ignore
 // import PropTypes from 'prop-types'
 import "./LatestNews.scss";
+import Ticker from "react-ticker";
+
+const News = () => (
+  <div className={"latest-news-container"}>
+    <div className="jumbotron-fluid" id={"breaking"}>
+      Latest News:
+    </div>
+    <div id={"newsTicker"}>
+      <Ticker mode="smooth" speed={15}>
+        {({ index }) => (
+          <div>
+            <h2 className="jumbotron-fluid">
+              This is the Headline of element {index}!<br />
+            </h2>
+          </div>
+        )}
+      </Ticker>
+    </div>
+  </div>
+);
 
 function LatestNews() {
   return (
-    <div className="latest-news-container">
-      <div>Latest News Allocated Space</div>
+    <div>
+      <News />
     </div>
   );
 }
-
-// LatestNews.propTypes = {
-
-// }
 
 export default LatestNews;
