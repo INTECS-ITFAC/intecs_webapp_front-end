aa5795b55afd6e631ad192384d5c5bd9d59a1445
Tashima <tashimadilrukshi@gmail.com>
2020-04-29 17:16:53 +0530

    Updated navbar tabs and navigation links.

---
+++ src/modules/project/IntecsOpenSource/IntecsOpenSourceProjects.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// import PropTypes from 'prop-types'
+import "./IntecsOpenSourceProjects.scss";
+
+export class IntecsOpenSourceProjects extends Component {
+  static propTypes = {};
+
+  render() {
+    return (
+      <div className="intecs-open-source-projects-container">
+        <div>INTECS Open Source Projects Allocated Space</div>
+      </div>
+    );
+  }
+}
+
+export default IntecsOpenSourceProjects;
