566423146d5db8633f81b1973bbcde71c1e435f2
Kelum Sampath Edirisinghe <edirisingheemks@gmail.com>
2020-05-09 13:56:30 +0530

    removed commented lines

--- src/modules/project/StudentsWork/StudentsWork.tsx
+++ src/modules/project/StudentsWork/StudentsWork.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-// import PropTypes from 'prop-types'
 import "./StudentsWork.scss";
 import Heading from "./headerSW/headersw";
-//import Heading from "../../../components/headingBanner/banner";
 
 export class StudentsWork extends Component {
   static propTypes = {};
