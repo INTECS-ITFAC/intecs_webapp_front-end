b926799aee37aec28c33427685a3fefd9956d9f1
Tashima <tashimadilrukshi@gmail.com>
2020-05-13 22:21:55 +0530

    Chaneged the navbar in "/contributors" page.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import Logo from "../../assets/images/intecs_logo.png";
 import "./Navbar.scss";
+import { useLocation } from "react-router-dom";
 
 function NavbarComponent() {
+  const location = useLocation();
+
   return (
     <Navbar
       bg="dark"
@@ -25,30 +28,38 @@ function NavbarComponent() {
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <div className="link-div">
-            <Nav.Link href="/">Home</Nav.Link>
-          </div>
-          <div className="link-div">
-            <Nav.Link href="/events">Events</Nav.Link>
-          </div>
-          <div className="dropdown">
-            <Nav.Link>Projects</Nav.Link>
-            <div className="dropdown-content">
-              <a href="/linkIT">LinkIT</a>
-              <a href="/INTECSOpenSourceProjects">
-                INTECS Open Source Projects
-              </a>
-              <a href="/studentsWork">Students' Work</a>
+        {location.pathname === "/contributes" ? (
+          <Nav className="ml-auto">
+            <div className="link-div">
+              <Nav.Link href="/">Back</Nav.Link>
             </div>
-          </div>
-          <div className="link-div">
-            <Nav.Link href="/gallery">Gallery</Nav.Link>
-          </div>
-          <div className="link-div">
-            <Nav.Link href="/about">About Us</Nav.Link>
-          </div>
-        </Nav>
+          </Nav>
+        ) : (
+          <Nav className="ml-auto">
+            <div className="link-div">
+              <Nav.Link href="/">Home</Nav.Link>
+            </div>
+            <div className="link-div">
+              <Nav.Link href="/events">Events</Nav.Link>
+            </div>
+            <div className="dropdown">
+              <Nav.Link>Projects</Nav.Link>
+              <div className="dropdown-content">
+                <a href="/linkIT">LinkIT</a>
+                <a href="/INTECSOpenSourceProjects">
+                  INTECS Open Source Projects
+                </a>
+                <a href="/studentsWork">Students' Work</a>
+              </div>
+            </div>
+            <div className="link-div">
+              <Nav.Link href="/gallery">Gallery</Nav.Link>
+            </div>
+            <div className="link-div">
+              <Nav.Link href="/about">About Us</Nav.Link>
+            </div>
+          </Nav>
+        )}
       </Navbar.Collapse>
     </Navbar>
   );
