b4e42e2f62bcb8143a83d73c178f1dd67c80a6fc
Sachin-Liyanage <sachinmamoru@gmail.com>
2020-05-18 11:51:55 +0530

    Add icons #120

--- src/components/sidebar/dashboardSidebar/DashboardSidebar.jsx
+++ src/components/sidebar/dashboardSidebar/DashboardSidebar.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import "./DashboardSidebar.scss";
 import { withRouter } from "react-router";
 import { Nav } from "react-bootstrap";
+import {
+  EventNote,
+  ListAlt,
+  School,
+  AccountTree,
+  AnnouncementOutlined,
+  BookOutlined,
+} from "@material-ui/icons";
 
 function DashboardSidebar(props) {
   return (
@@ -12,22 +20,22 @@ function DashboardSidebar(props) {
         activeKey={props.location.pathname}
       >
         <Nav.Link href="/dashboard/eventManagement" className="link-div">
-          Event Management
+          <EventNote /> Event Management
         </Nav.Link>
         <Nav.Link href="/dashboard/eventFlowManagement" className="link-div">
-          Flow Management
+          <ListAlt /> Flow Management
         </Nav.Link>
         <Nav.Link href="/dashboard/newsManagement" className="link-div">
-          News Management
+          <AnnouncementOutlined /> News Management
         </Nav.Link>
         <Nav.Link href="/dashboard/linkIT" className="link-div">
-          LinkIT
+          <BookOutlined /> LinkIT
         </Nav.Link>
         <Nav.Link href="/dashboard/addStudentWork" className="link-div">
-          Add Student Work
+          <School /> Add Student Work
         </Nav.Link>
         <Nav.Link href="/dashboard/openSourceManagement" className="link-div">
-          Open Source Management
+          <AccountTree /> Open Source Management
         </Nav.Link>
       </Nav>
     </div>
